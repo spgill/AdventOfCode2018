5b40b62e68466c7fb77489a129613c881af98d86
Samuel P. Gillispie II <Samuel.Gillispie@ibm.com>
2018-12-01 13:29:32 -0600

    Solutions for day 1

--- day01/puzzle1.py
+++ day01/puzzle1.py
@@ -1,1 +1,10 @@
-print('I\'m empty!')
+import pathlib
+
+
+data = (pathlib.Path(__file__).parent / 'puzzle1-input.txt').open('r').read()
+
+output = sum(
+    [int(n) for n in data.splitlines()]
+)
+
+print('OUTPUT:', output)
--- day01/puzzle2.py
+++ day01/puzzle2.py
@@ -1,1 +1,19 @@
-print('I\'m empty!')
+import itertools
+import pathlib
+
+
+data = (pathlib.Path(__file__).parent / 'puzzle1-input.txt').open('r').read()
+
+numbers = [int(n) for n in data.splitlines()]
+
+frequency = 0
+seen = {0}
+
+for num in itertools.cycle(numbers):
+    frequency += num
+
+    if frequency in seen:
+        print('DUPLICATE FOUND:', frequency)
+        break
+
+    seen.add(frequency)
