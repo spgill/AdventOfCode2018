9a0e912456e71337a25b8a1b230f5081d1294ca4
Samuel P. Gillispie II <samuel@spgill.me>
2018-12-02 18:14:20 -0600

    Day 2 solutions

--- day02/puzzle1.py
+++ day02/puzzle1.py
@@ -1,1 +1,29 @@
-print('I\'m empty!')
+import pathlib
+import re
+
+dataPath = pathlib.Path(__file__).parent / 'input.txt'
+data = dataPath.open('r').read()
+
+abet = 'abcdefghijklmnopqrstuvwxyz'
+
+doubleCount = 0
+for line in data.splitlines():
+    for char in abet:
+        found = re.findall(char, line)
+        count = len(found)
+
+        if count == 2:
+            doubleCount += 1
+            break
+
+tripleCount = 0
+for line in data.splitlines():
+    for char in abet:
+        found = re.findall(char, line)
+        count = len(found)
+
+        if count == 3:
+            tripleCount += 1
+            break
+
+print('CHECKSUM', doubleCount * tripleCount)
--- day02/puzzle2.py
+++ day02/puzzle2.py
@@ -1,1 +1,22 @@
-print('I\'m empty!')
+import itertools
+import pathlib
+
+dataPath = pathlib.Path(__file__).parent / 'input.txt'
+data = dataPath.open('r').read()
+
+
+def difference(lineA, lineB):
+    diff = []
+    for i in range(len(lineA)):
+        if lineA[i] != lineB[i]:
+            diff.append(i)
+    return diff
+
+
+for lineA, lineB in itertools.combinations(data.splitlines(), 2):
+    diff = difference(lineA, lineB)
+    if len(diff) == 1:
+        diff = diff[0]
+        common = lineA[:diff] + lineA[diff + 1:]
+        print('COMMON CHARACTERS:', common)
+        break
