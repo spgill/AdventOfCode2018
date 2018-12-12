6e9c9932751ba2b004f3b75177fad22c2986103b
Samuel P. Gillispie II <Samuel.Gillispie@ibm.com>
2018-12-12 14:49:19 -0600

    Day 5 solutions

--- day05/puzzle1.py
+++ day05/puzzle1.py
@@ -1,1 +1,22 @@
-print('I\'m empty!')
+import pathlib
+
+
+# Parse the raw input data
+chain = (pathlib.Path(__file__).parent / 'input.txt').open('r').read().strip()
+
+# Create a list of replacement strings from characters in the alphabet
+abet = 'abcdefghijklmnopqrstuvwxyz'
+replacements = []
+for char in abet:
+    replacements.append(char.upper() + char)
+    replacements.append(char + char.upper())
+
+chainLen = len(chain)
+while True:
+    for repl in replacements:
+        chain = chain.replace(repl, '')
+
+    if len(chain) == chainLen:
+        print(f'The answer is {len(chain)}')
+        break
+    chainLen = len(chain)
--- day05/puzzle2.py
+++ day05/puzzle2.py
@@ -1,1 +1,32 @@
-print('I\'m empty!')
+import pathlib
+
+
+# Parse the raw input data
+chain = (pathlib.Path(__file__).parent / 'input.txt').open('r').read().strip()
+
+# Create a list of replacement strings from characters in the alphabet
+abet = 'abcdefghijklmnopqrstuvwxyz'
+replacements = []
+for char in abet:
+    replacements.append(char.upper() + char)
+    replacements.append(char + char.upper())
+
+attempts = []
+for attempt in abet:
+    attemptChain = chain
+    for repl in [attempt, attempt.upper()]:
+        attemptChain = attemptChain.replace(repl, '')
+
+    chainLen = len(attemptChain)
+    while True:
+        for repl in replacements:
+            attemptChain = attemptChain.replace(repl, '')
+
+        if len(attemptChain) == chainLen:
+            attempts.append(len(attemptChain))
+            break
+        chainLen = len(attemptChain)
+
+print(
+    f'The answer is {min(attempts)}'
+)
